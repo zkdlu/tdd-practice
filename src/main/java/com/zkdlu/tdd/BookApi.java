4c09d86226883d821ffb50d5a968c69a92866e88
zkdlu <zkdlu159951@gmail.com>
2022-01-19 20:05:41 +0900

    test for empty list from get endpoint

--- src/main/java/com/zkdlu/tdd/BookApi.java
+++ src/main/java/com/zkdlu/tdd/BookApi.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/books")
 public class BookApi {
     @GetMapping
-    public void getBooks() {
-
+    public List<String> getBooks() {
+        return new ArrayList<>();
     }
 }
--- src/test/java/com/zkdlu/tdd/BookApiTest.java
+++ src/test/java/com/zkdlu/tdd/BookApiTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class BookApiTest {
@@ -16,5 +18,15 @@ class BookApiTest {
 
         mockMvc.perform(get("/api/books"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void test_getBookApi_returnsAnEmptyList() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders
+                .standaloneSetup(new BookApi())
+                .build();
+
+        mockMvc.perform(get("/api/books"))
+                .andExpect(jsonPath("$", hasSize(0)));
     }
 }
