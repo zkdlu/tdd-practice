1185c93b446913a923cc6b30cf313320a360818c
zkdlu <zkdlu159951@gmail.com>
2022-01-19 22:18:04 +0900

    notice to spring missing a repository

---
+++ src/main/java/com/zkdlu/tdd/LocalBookRepository.java
@@ -0,0 +1,23 @@
+package com.zkdlu.tdd;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class LocalBookRepository implements BookRepository {
+    @Override
+    public List<Book> getAll() {
+        return null;
+    }
+
+    @Override
+    public Book getById(long id) {
+        return null;
+    }
+
+    @Override
+    public Book create(NewBook newBook) {
+        return null;
+    }
+}
